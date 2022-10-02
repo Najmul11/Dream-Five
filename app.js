d5dfa4362ba7840d543071a1a3b1e430aa39d2cb
NazPro <najmulbinnurul3@gmail.com>
2022-10-02 20:30:57 +0600

    last two push

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// -------select button handler----------------
 let count=0
 function buttonHandle(id1,id2,id3) {
    if (count<5) {
@@ -17,3 +18,31 @@ function buttonHandle(id1,id2,id3) {
     alert('Already 5 players selected');
    }
 }
+
+//-------- calcultetion handler------------
+const getInputValue=id=>{
+    const inputField=document.getElementById(id);
+    const inputValue=parseFloat(inputField.value);
+    inputField.value=''
+    return inputValue;
+}
+const setTotal=(id,amount)=>{
+    const element=document.getElementById(id);
+    element.innerText=amount;
+}
+const calculateBtn=document.getElementById('calculate-btn')
+calculateBtn.addEventListener('click',()=>{
+    const perPlayerPrice=getInputValue('price-input')
+    const playersTotalExpense=perPlayerPrice * count;
+    const playersTotal=setTotal('players-total',playersTotalExpense)
+})
+
+const totalCalc=document.getElementById('total-calc')
+totalCalc.addEventListener('click',()=>{
+    const managerCost=getInputValue('manager-cost')
+    const coachCost=getInputValue('coach-cost')
+    const element=document.getElementById('players-total');
+    const elementText=parseFloat(element.innerText);
+    const totalCost=elementText + managerCost + coachCost;
+    const playersTotal=setTotal('total-expense',totalCost)
+})
